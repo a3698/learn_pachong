2d07b03a7476a6b14f62548a5daa24add01a19b0
Y_en <ye5286@163.com>
2019-07-07 22:29:54 +0800

    1

--- random_user_agent.py
+++ random_user_agent.py
@@ -7,7 +7,8 @@ def load_baidu():
     user_agent_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1",
         "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0",
-        "Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50"
+        "Opera/9.80 (Windows NT 6.1; U; zh-cn) Presto/2.9.168 Version/11.50",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
     ]
 
     random_user_agent = random.choice(user_agent_list)
